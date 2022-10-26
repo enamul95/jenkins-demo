2b3bcadd77dacf2b2fa31910ebd339aa94d85887
enamul95 <ehaque95@gmail.com>
2022-10-26 11:21:45 +0600

    add packing war

--- src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,9 +2,16 @@ package com.example.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsDemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);

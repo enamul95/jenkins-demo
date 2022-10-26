3908e8d7043f6172b61b7b7815707f78002faa6a
enamul95 <ehaque95@gmail.com>
2022-10-26 10:47:29 +0600

    first commit

---
+++ src/main/java/com/example/jenkinsdemo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.jenkinsdemo.controller;
+
+import com.example.jenkinsdemo.responsemodel.ResponseModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home/")
+public class HomeController {
+
+    @GetMapping("/test")
+    public ResponseModel getMessage(){
+        ResponseModel model = new ResponseModel();
+        model.setResponseCode("0");
+        model.setResponseMessage("Welcome to Jenkins Demo");
+       return model;
+
+    }
+}
---
+++ src/main/java/com/example/jenkinsdemo/responsemodel/ResponseModel.java
@@ -0,0 +1,28 @@
+package com.example.jenkinsdemo.responsemodel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class ResponseModel {
+
+    private String responseCode;
+    private String responseMessage;
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
